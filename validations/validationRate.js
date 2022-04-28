3bdfbc0f2aa7c3384d42e58b04fb55d2f0ddc452
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-04-28 19:31:39 -0300

    requisito 06

--- validations/validationRate.js
+++ validations/validationRate.js
@@ -1,5 +1,5 @@
 const validationRate = (req, res, next) => {
-    const { talk: { rate } } = req.body;
+    const { talk: rate } = req.body;
     
         if (rate === undefined || !(Number.isInteger(rate)) || rate < 1 || rate > 5) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
--- validations/validationTalk.js
+++ validations/validationTalk.js
@@ -1,6 +1,6 @@
 const validationTalk = (req, res, next) => {
    const { talk } = req.body;
-        if (!talk || !talk.rate || !talk.watchedAt) {
+        if (!talk || !talk.watchedAt) {
     return res.status(400).json(
         { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
 );
