a5810b3be8db7ab1a65d90354186b8be5ee58ca0
João Keuwe <91097315+JoaoKeuwe@users.noreply.github.com>
2022-04-28 19:42:24 -0300

    k

--- validations/validationTalk.js
+++ validations/validationTalk.js
@@ -1,12 +1,11 @@
 const validationTalk = (req, res, next) => {
-    const MENSAGEM = {
-        message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
-      };
-          const { talk } = req.body;
-          if (talk === undefined) return res.status(400).json(MENSAGEM);
-          if (talk.rate === undefined) return res.status(400).json(MENSAGEM);
-          if (talk.watchedAt === undefined) return res.status(400).json(MENSAGEM);
-          next();
-      };
-      
-module.exports = validationTalk;
+   const { talk } = req.body;
+        if (!talk || !talk.watchedAt) {
+    return res.status(400).json(
+        { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
+);
+        }
+       return next();
+    };
+    
+    module.exports = validationTalk;
